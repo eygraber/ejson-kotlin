e236c8ef7aa33ebf40942fd424e351698fe9bc17
Eliezer Graber <me@eygraber.com>
2023-01-18 22:04:01 -0500

    Migrate to gradle-conventions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,73 @@
+import com.eygraber.conventions.Env
+import com.eygraber.conventions.repositories.addCommonRepositories
+
+pluginManagement {
+  repositories {
+    google {
+      content {
+        includeGroupByRegex("com\\.google.*")
+        includeGroupByRegex("com\\.android.*")
+        includeGroupByRegex("androidx.*")
+      }
+    }
+
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev") {
+      content {
+        includeGroupByRegex("org\\.jetbrains.*")
+      }
+    }
+
+    mavenCentral()
+
+    maven("https://oss.sonatype.org/content/repositories/snapshots") {
+      mavenContent {
+        snapshotsOnly()
+      }
+    }
+
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots") {
+      mavenContent {
+        snapshotsOnly()
+      }
+    }
+
+    gradlePluginPortal()
+  }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+  // comment this out for now because it doesn't work with KMP js
+  // https://youtrack.jetbrains.com/issue/KT-51379/
+  // repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+  repositories {
+    addCommonRepositories(
+      includeMavenCentral = true,
+      includeMavenCentralSnapshots = true,
+      includeGoogle = true,
+      includeJetbrainsCompose = true
+    )
+  }
+}
+
+plugins {
+  id("com.eygraber.conventions.settings") version "0.0.29"
+  id("com.gradle.enterprise") version "3.12.2"
+  id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+}
+
 rootProject.name = "ejson-kotlin"
-include("ejson")
-include("ejson-gradle")
+
+include(":ejson")
+include(":ejson-gradle")
+
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    if(Env.isCI) {
+      termsOfServiceAgree = "yes"
+      publishAlways()
+    }
+  }
+}

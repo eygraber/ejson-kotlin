778a56a2cb6dcfefb58517bb8de0677cfe8791c4
Eliezer Graber <me@eygraber.com>
2024-05-21 23:55:32 -0400

    Kotlin 2.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,12 +11,6 @@ pluginManagement {
       }
     }
 
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev") {
-      content {
-        includeGroupByRegex("org\\.jetbrains.*")
-      }
-    }
-
     mavenCentral()
 
     maven("https://oss.sonatype.org/content/repositories/snapshots") {
@@ -37,22 +31,19 @@ pluginManagement {
 
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-  // comment this out for now because it doesn't work with KMP js
-  // https://youtrack.jetbrains.com/issue/KT-51379/
-  // repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 
   repositories {
     addCommonRepositories(
       includeMavenCentral = true,
       includeMavenCentralSnapshots = true,
       includeGoogle = true,
-      includeJetbrainsCompose = true,
     )
   }
 }
 
 plugins {
-  id("com.eygraber.conventions.settings") version "0.0.72"
+  id("com.eygraber.conventions.settings") version "0.0.73"
   id("com.gradle.develocity") version "3.17.4"
 }
 
